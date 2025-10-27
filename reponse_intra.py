7c213c634401dd939f951de70b58d126b9a52832
camplourde <2480389@etu.cegepsth.qc.ca>
2025-10-27 12:16:44 -0400

    test de push

--- reponse_intra.py
+++ reponse_intra.py
@@ -1,3 +1,44 @@
 ##########################################
-# Nom, Prénom, <Numéro de DA>
+# Plourde, Camille, <2480389>
 ##########################################
+
+# Question 1
+
+import random as rd
+def question1():
+    for i in range(10):
+        temperature = rd.uniform(20, 35)
+        temperature_arrondie = round(temperature, 1)
+        jours = 0
+        print("Jour", i + 1, ":", temperature_arrondie)
+        if temperature_arrondie > 30:
+            print("Trop chaud")
+        elif temperature_arrondie < 24:
+            print("Trop froid")
+        else:
+            print("OK")
+    print("Fin")
+question1()
+
+# Question 2
+
+import numpy as np
+import matplotlib.pyplot as plt
+def question2():
+    population_bacteries_initiale = int(input("Entrez la population de bactéries initiale :"))
+    for i in range(10):
+        heures = 0
+        taux_augmentation = np.pi/1.5
+        croissance_population_bacteries = population_bacteries_initiale * taux_augmentation
+
+    plt.figure(figsize=(5, 5))
+    plt.title("Croissance bactérienne")
+    plt.xlabel("Heures")
+    plt.ylabel("Population")
+    plt.xlim(0, 10)
+    plt.ylim(0, 160000)
+    plt.plot([heures, croissance_population_bacteries], "*")
+    plt.grid()
+    plt.show()
+
+question2()
